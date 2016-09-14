7c69cfd575950cecb7de32258c8734745b85dc45
mdymen <msn@dymenstein.com>
2016-09-14 06:51:31 -0300

    xx
    x

--- application/modules/default/controllers/RegisterController.php
+++ application/modules/default/controllers/RegisterController.php
@@ -101,7 +101,7 @@ class RegisterController extends Zend_Controller_Action
                 'team2' => $id2,
                 'date' => date('d-n-y'),
                 'championship' => $params['championship'],
-                'round' => 1
+                'round' => 2
             ));
             $j = $j + 1;
         }
